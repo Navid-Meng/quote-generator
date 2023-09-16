dfbf9aeaf7f812c94f9de7cd45f5c21451c5685a
Vid3107 <navidmeng3107@gmail.com>
2023-09-16 19:48:34 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+var apiQuote = '';
+
+function newQuote(){
+    if (apiQuote['author'] == null){
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = apiQuote['author'];
+    }
+
+    if (apiQuote['content'].length > 100){
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = apiQuote['content'];
+}
+
+async function getQuotes(){
+    const apiUrl = 'https://api.quotable.io/random';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuote = await response.json();
+        newQuote();
+    } catch (error){
+        alert('An error happens when fetching');
+    }
+}
+
+newQuoteBtn.addEventListener('click', getQuotes);
+
+getQuotes();
